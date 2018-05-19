0fc5d5f784557c46417efb2ec7fc5b0a71f4db04
ksysiekj <jakielaszek.krzysztof@gmail.com>
2018-05-19 21:22:30 +0200

    Add project files.

---
+++ billing-enterprise-api-usage-detail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace billing_enterprise_api_usage_detail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
